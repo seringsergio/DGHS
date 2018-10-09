c9aea78b9feb528fe96e25a684648537f31b10a3
seringsergio <diaz-sergio@javeriana.edu.co>
2018-10-09 15:15:22 -0500

    PPR bien CITT y RPL

--- BayesTraining/matlabScript/readFiles_latency_PRR.m
+++ BayesTraining/matlabScript/readFiles_latency_PRR.m
@@ -1,15 +1,15 @@
 %% sink int_0 citt
 
-seqno_sink_citt_int_0_7dBm        = xlsread('/home/seringsergio/Desktop/DGHS/results/9_nodes/Latency_PRR/latency_PRR_sink_citt_int_0_7dBm.xlsx','latency_PRR_sink_citt_int_0_7dBm','B2:B502');
-nodeID_sink_citt_int_0_7dBm       = xlsread('/home/seringsergio/Desktop/DGHS/results/9_nodes/Latency_PRR/latency_PRR_sink_citt_int_0_7dBm.xlsx','latency_PRR_sink_citt_int_0_7dBm','C2:C502');
-[temp, date_sink_citt_int_0_7dBm] = xlsread('/home/seringsergio/Desktop/DGHS/results/9_nodes/Latency_PRR/latency_PRR_sink_citt_int_0_7dBm.xlsx','latency_PRR_sink_citt_int_0_7dBm','D2:D502');
+seqno_sink_citt_int_0_7dBm        = xlsread('/home/seringsergio/Desktop/DGHS/results/9_nodes/Latency_PRR/latency_PRR_sink_citt_int_0_7dBm.xlsx','latency_PRR_sink_citt_int_0_7dBm','B2:B149');
+nodeID_sink_citt_int_0_7dBm       = xlsread('/home/seringsergio/Desktop/DGHS/results/9_nodes/Latency_PRR/latency_PRR_sink_citt_int_0_7dBm.xlsx','latency_PRR_sink_citt_int_0_7dBm','C2:C149');
+[temp, date_sink_citt_int_0_7dBm] = xlsread('/home/seringsergio/Desktop/DGHS/results/9_nodes/Latency_PRR/latency_PRR_sink_citt_int_0_7dBm.xlsx','latency_PRR_sink_citt_int_0_7dBm','D2:D149');
 
 
 %% leaf node int_0 citt
 
-seqno_leaf_citt_int_0_7dBm        = xlsread('/home/seringsergio/Desktop/DGHS/results/9_nodes/Latency_PRR/latency_PRR_leaf_citt_int_0_7dBm.xlsx','latency_PRR_leaf_citt_int_0_7dBm','B2:B4330');
-nodeID_leaf_citt_int_0_7dBm       = xlsread('/home/seringsergio/Desktop/DGHS/results/9_nodes/Latency_PRR/latency_PRR_leaf_citt_int_0_7dBm.xlsx','latency_PRR_leaf_citt_int_0_7dBm','C2:C4330');
-[temp, date_leaf_citt_int_0_7dBm] = xlsread('/home/seringsergio/Desktop/DGHS/results/9_nodes/Latency_PRR/latency_PRR_leaf_citt_int_0_7dBm.xlsx','latency_PRR_leaf_citt_int_0_7dBm','D2:D4330');
+seqno_leaf_citt_int_0_7dBm        = xlsread('/home/seringsergio/Desktop/DGHS/results/9_nodes/Latency_PRR/latency_PRR_leaf_citt_int_0_7dBm.xlsx','latency_PRR_leaf_citt_int_0_7dBm','B2:B420');
+nodeID_leaf_citt_int_0_7dBm       = xlsread('/home/seringsergio/Desktop/DGHS/results/9_nodes/Latency_PRR/latency_PRR_leaf_citt_int_0_7dBm.xlsx','latency_PRR_leaf_citt_int_0_7dBm','C2:C420');
+[temp, date_leaf_citt_int_0_7dBm] = xlsread('/home/seringsergio/Desktop/DGHS/results/9_nodes/Latency_PRR/latency_PRR_leaf_citt_int_0_7dBm.xlsx','latency_PRR_leaf_citt_int_0_7dBm','D2:D420');
 
 % %% sink int_0 RPL
 % 
